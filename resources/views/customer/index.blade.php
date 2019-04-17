7ee577e8c6df89994fd4654787c3643470ca35d2
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-04-17 18:16:30 +0900

    顧客リストの詳細リンク削除

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,22 +1,21 @@
     <h1>顧客リスト画面</h1>
     <div>
         <table style="width:100%;" border="1">
-            <tr><th>ニックネーム</th><th>性別</th><th>詳細</th></tr>
+            <tr><th>ニックネーム</th><th>性別</th></tr>
         @foreach($customers as $customer)
             <tr>
-            <td>{{$customer->nickname}}</td>
-            @if( $customer->gender == "1")
+                <td>{{$customer->nickname}}</td>
+                @if( $customer->gender == "1")
                 <td>男性</td>
-            @else
+                @else
                 <td>女性</td>
-            @endif
-            <td>
-            <form action="/customer/detail" method="POST">
-                {{ csrf_field() }}
-                <input type="hidden" name="id" value="{{$customer->id}}">
-                <input type="submit" value="詳細へ">
-            </form>
-            </td>
+                @endif
+                <td>
+                    <form action="/customer/detail" method="POST">
+                        {{ csrf_field() }}
+                        <input type="hidden" name="id" value="{{$customer->id}}">
+                    </form>
+                </td>
             </tr>
         @endforeach
         </table>
