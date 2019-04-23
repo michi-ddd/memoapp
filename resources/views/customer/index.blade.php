8fa688affa531fcaaae541a97a9acb63c3bb4a2f
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-04-23 18:55:28 +0900

    コードを読みやすく

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,21 +1,22 @@
     <h1>顧客リスト画面</h1>
     <div>
         <table style="width:100%;" border="1">
-            <tr><th>ニックネーム</th><th>性別</th></tr>
-        @foreach($customers as $customer)
+            <tr>
+                <th>ニックネーム</th>
+                <th>性別</th>
+            </tr>
+            @foreach($customers as $customer)
             <tr>
                 <td>
                     <a href="/customer/show/{{$customer->id}}">{{$customer->nickname}}</a>
                 </td>
-
                 @if( $customer->gender == "1")
                 <td>男性</td>
                 @else
                 <td>女性</td>
                 @endif
-                
             </tr>
-        @endforeach
+            @endforeach
         </table>
     </div>
     <br>
