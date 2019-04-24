49dd92c66de8cb6b9d8074e8862c31f37c46e949
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-04-24 11:20:20 +0900

    メモ削除 form action先を修正

--- resources/views/memo/del.blade.php
+++ resources/views/memo/del.blade.php
@@ -1,7 +1,7 @@
    
 <h1>Memo削除画面</h1>
 <div>
-    <form action="/memo/edit" method="POST">
+    <form action="/memo/del" method="POST">
     <table style="width:100%;"cellpadding="3" border="1">
     {{ csrf_field() }}
     <input type="hidden" name="id" value="{{$memo->id}}">
