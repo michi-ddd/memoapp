d1cd49e6c6aa50b97655927d6b34c803f4d53ccd
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-05-02 11:29:55 +0900

    Timeline 画面修正

--- resources/views/memo/index.blade.php
+++ resources/views/memo/index.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>メモリスト</h1>
-    <div>
-        <table style="width:100%;" border="1">
-            <tr>
-                <th>ニックネーム</th>
-                <th>メモ</th>
-                <th>投稿者</th>
-                <th>投稿日</th>
-            </tr>
-            @foreach($memos as $memo)
-            <tr>
-                <td>{{$memo->customer->nickname}}</td>
-                <td>{{$memo->text}}</td>
-                <td>{{$memo->user->name}}</td>
-                <td>{{$memo->created_at}}</td>
-            </tr>
+<div class="container">
+    <div class="card">
+        <div class="h3 card-header">
+            Timeline
+        </div>
+        <ul class="list-group list-group-flush">
+            @foreach ($memos as $memo)
+            <div class="card-body list-group-item">
+                <div class="card-title text-center card-title border-bottom">
+                    {{$memo->customer->nickname}}'s memo
+                </div>
+                <div class="card-text">
+                    {{$memo->text}}
+                </div>
+                <div class="text-right mb-2">
+                    <a href="/memo/edit/{{$memo->id}}" class="btn-sm btn-secondary small">Edit</a>
+                    <a href="/memo/del/{{$memo->id}}" class="btn-sm btn-secondary small">Delete</a>
+                </div>
+                <div class="text-right text-muted h6">
+                    from:{{$memo->user->name}} ({{$memo->created_at->format('Y,m,d H:i')}})
+                </div>      
+            </div>
             @endforeach
-        </table>
+        </ul>
     </div>
+</div>
 @endsection
