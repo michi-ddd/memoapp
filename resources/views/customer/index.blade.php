a32c5efa75bad90993bf5f5e7f0feba263184721
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-05-02 10:38:03 +0900

    顧客リスト画面修正

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -2,25 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="card" style="width: 18rem;">
-            <div class="card-header">Customer List</div>
-                <ul class="list-group list-group-flush">
-                @foreach($customers as $customer)
-                    <a class="list-group-item" href="/customer/show/{{$customer->id}}">{{$customer->nickname}}
-                    <div class="text-muted small">
-                    [性別：
-                    @if( $customer->gender == "1")
-                    男性
-                    @else
-                    女性
-                    @endif
-                    ]
-                    </div>
-                    </a>
-                @endforeach
-                    </ul>
-            </div>
+    <div class="card">
+        <div class="card-header h3">Customer List</div>
+            <ul class="list-group list-group-flush">
+            @foreach($customers as $customer)
+                <a class="h6 list-group-item" href="/customer/show/{{$customer->id}}">{{$customer->nickname}}
+                <span class="text-muted small">(
+                @if( $customer->gender == "1")Men
+                @else Women
+                @endif
+                )</span>
+                </a>
+            @endforeach
+            </ul>
         </div>
     </div>
 </div>
