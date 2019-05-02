07b35303a82ad5459d937d02ff882241c5682d55
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-05-02 11:40:40 +0900

    メモ編集画面修正

--- resources/views/memo/edit.blade.php
+++ resources/views/memo/edit.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Memo編集画面</h1>
-<div>
+<div class="card">
+  <div class="h3 card-header">Memo Edit</div>
+  @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+        </ul>
+    </div>
+  @endif
   <form action="/memo/edit" method="POST">
   {{ csrf_field() }}
-      <table style="width:100%;"cellpadding="3" border="1">
-          <input type="hidden" name="id" value="{{$memo->id}}">
-          <tr>
-            <th>名前</th>
-            <td>{{$memo->customer->nickname}}</td>
-          </tr>
-          <tr>
-            <th>Memo</th>
-            <td><textarea  name="text" cols="40" rows="4" maxlength="20" placeholder="{{$memo->text}}"></textarea></td>
-        </table>
-    <input type="submit">
+      <div class="container">
+        <input type="hidden" name="id" value="{{$memo->id}}">
+        <div class="form-group mt-1">
+          <label>Please edit the note</label>
+          <textarea type="text" name="text" class="form-control" value="{{$memo->text}}">{{$memo->text}}</textarea>
+        </div>
+        <div class="text-muted h6">
+            from:{{$memo->user->name}} ({{$memo->created_at->format('Y,m,d H:i')}}) 
+             <button class="btn-secondary float-right mb-1" type="submit">Edit</button>
+        </div>
+      </div>
   </form>
 </div>
+
 @endsection
