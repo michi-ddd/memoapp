7e0472aaecf60a97bcac4a3b06ca7796b19cc253
Michihiro　Doi <48142612+michi-ddd@users.noreply.github.com>
2019-05-02 10:08:25 +0900

    詳細画面デザイン修正

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -2,33 +2,35 @@
 
 @section('content')
 <div class="container">
-<div class="card-header">Profile</div>
-
+    <div class="card-header">Profile</div>
         <div class="card-body h3">
-        {{$customer->nickname}}
-        <a href="/memo/create/{{$customer->id}}" class="btn btn-light small">Post</a>
-        <a href="/customer/edit/{{$customer->id}}" class="btn btn-light small">Edit</a>
-        <a href="/customer/del/{{$customer->id}}" class="btn btn-light small">Delete</a>
-            <div class="text-muted h6">
-                [性別：
-                @if( $customer->gender == "1")
-                男性
-                @else
-                女性
-                @endif
-                ]
-            </div>
+            {{$customer->nickname}}
+            <span class="text-muted h6">
+                    [性別：
+                    @if( $customer->gender == "1")
+                    男性
+                    @else
+                    女性
+                    @endif
+                    ]
+            </span>
+        <div class="text-right">
+            <a href="/memo/create/{{$customer->id}}" class="btn-sm btn-secondary small">Post</a>
+            <a href="/customer/edit/{{$customer->id}}" class="btn-sm btn-secondary small">Edit</a>
+            <a href="/customer/del/{{$customer->id}}" class="btn-sm btn-secondary small">Delete</a>
         </div>
-</table>
- <div class="card-header">Customers Memo</div>
+    </div>
+    <div class="card-header">Memo</div>
         @foreach ($customer->memos as $memo)
-        <div class="card-body">
+        <div class="card-body border-bottom">
             {{$memo->text}}
-            <a href="/memo/edit/{{$memo->id}}" class="btn btn-light small">Edit</a>
-            <a href="/memo/del/{{$memo->id}}" class="btn btn-light small">Delete</a>
-            <div class="text-muted h6">from:{{$memo->user->name}} at:{{$memo->created_at->format('Ymd Hi')}}</td>
+            <div class="text-right p-1">
+                <a href="/memo/edit/{{$memo->id}}" class="btn-sm btn-secondary small">Edit</a>
+                <a href="/memo/del/{{$memo->id}}" class="btn-sm btn-secondary small">Delete</a>
+            </div>
+            <div class="text-right text-muted h6">from:{{$memo->user->name}} ({{$memo->created_at->format('Y,m,d H:i')}})</div>
         </div>
         @endforeach
-    </table>
+    </div>
 </div>
 @endsection
